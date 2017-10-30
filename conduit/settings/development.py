37540bdd4fde4dba82d8be806b58244e5ba7b641
Christopher Paul <christopherp@mindfiresolutions.com>
2017-10-30 23:40:07 +0530

    Added a few apps and tested some of them. Needs testing and more configuration before moving to staging branch

---
+++ conduit/settings/development.py
@@ -0,0 +1,61 @@
+from conduit.settings.common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2^f+3@v7$v1f8yt0!s)3-1t$)tlp+xm17=*g))_xoi&&9m#2a&'
+
+ALLOWED_HOSTS = (
+    'localhost',
+    '127.0.0.1',
+)
+
+INSTALLED_APPS += (
+    'django_extensions',
+    'rest_framework_swagger',
+)
+
+LOGIN_URL = '/api-auth/login/'
+LOGOUT_URL = '/api-auth/logout/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
+    'rest_framework.authentication.SessionAuthentication',
+    'conduit.apps.authentication.backends.JWTAuthentication',
+)
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        },
+        'permission_denied_handler': 'django.contrib.auth.views.login',
+        'is_authenticated': True,  # Set to True to enforce user authentication,
+        'is_superuser': True  # Set to True to enforce admin only access
+    }
+}
+
+# DATABASE SETTINGS
+DATABASES = {
+    'default': {
+        'NAME': 'realworld_django',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'mindfire',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+CORS_ORIGIN_WHITELIST = ()
+
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
---
+++ conduit/settings/staging.py
@@ -0,0 +1,47 @@
+from conduit.settings.common import *
+import dj_database_url
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+ALLOWED_HOSTS = ()
+
+INSTALLED_APPS += (
+    'django_extensions',
+    'rest_framework_swagger',
+)
+
+LOGIN_URL = '/api-auth/login/'
+LOGOUT_URL = '/api-auth/logout/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = (
+    'rest_framework.authentication.SessionAuthentication',
+    'conduit.apps.authentication.backends.JWTAuthentication',
+)
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        },
+        'permission_denied_handler': 'django.contrib.auth.views.login',
+        'is_authenticated': True,  # Set to True to enforce user authentication,
+        'is_superuser': True  # Set to True to enforce admin only access
+    }
+}
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+
+CORS_ORIGIN_WHITELIST = ()
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
